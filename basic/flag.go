8f384eb9544ff08282074909ef38f5fc32c4d340
caojinyan <caojinyan24@163.com>
2018-09-24 23:49:33 +0800

    flag

--- basic/flag.go
+++ basic/flag.go
@@ -6,13 +6,16 @@ import (
 	"strings"
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", "$", "seperator")
+var n = flag.Bool("n", true, "omit trailing newline")
+var sep = flag.String("s", " ", "seperator")
 
 func FlagMain() {
 	flag.Parse()
+	//flag.Args()输出没有指定tag的变量，`go run main.go -n a bc def`中，flag.Args()=[a bc def]
+	fmt.Println(flag.Args())
+	fmt.Println(*sep, *n)
 	fmt.Println(strings.Join(flag.Args(), *sep))
-	fmt.Println("n:",!*n)
+	fmt.Println("!n:", !*n)
 	if !*n {
 		fmt.Println("end")
 	}
